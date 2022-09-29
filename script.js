ef814cebddaa571c620aa138306d66d449526ab5
boriyevv08 <manoken999@gmail.com>
2022-09-29 15:26:54 +0500

    Lesson3 Homework

---
+++ script.js
@@ -0,0 +1,184 @@
+"use strict";
+
+//     M1
+
+// let day = +prompt(
+//   "Hafta kunini tanlash uchun 1 dan 7 gacha bo'lgan ixtiyoriy raqam kiriting"
+// );
+// {
+//   switch ("day") {
+//     case 1:
+//       console.log("Dushanba");
+//       break;
+//     case 2:
+//       console.log("Seshanba");
+//       break;
+//     case 3:
+//       console.log("Chorshanba");
+//       break;
+//     case 4:
+//       console.log("Payshanba");
+//       break;
+//     case 5:
+//       console.log("Juma");
+//       break;
+//     case 6:
+//       console.log("Shanba");
+//       break;
+//     case 7:
+//       console.log("Yakshanba");
+//       break;
+//     default:
+//       console.log("Bunday hafta kuni mavjud emas");
+//   }
+// }
+
+// M 2
+
+// let Baxo = +prompt(
+//   "Baholash uchun 1(yomon) dan 5(a'lo) gacha bo'lgan ixtiyoriy raqamni tanlang "
+// );
+// {
+//   switch (Baxo) {
+//     case 1:
+//       console.log("Yomon");
+//       break;
+//     case 2:
+//       console.log("Qoniqarsiz");
+//       break;
+//       case 3:
+//       console.log("Qoniqarli");
+//       break;
+//       case 4:
+//       console.log("Yaxshi");
+//       break;
+//       case 5:
+//       console.log("A'lo");
+//       break;
+//       default:
+//       console.log("Bunday baho mavjud emas");
+//   }
+//
+
+//  M 3
+
+// let month =+prompt("Yil oylari raqamini kiriting, men esa sizga uni qaysi faslda ekaligini aytaman!");
+
+// {
+//       switch (month){
+//             case 12: case 1: case 2:
+//                   console.log("Qish");
+//                   break;
+//                   case 3: case 4: case 5:
+//                   console.log("Bahor");
+//                   break;
+//                   case 6: case 7: case 8:
+//                   console.log("Yoz");
+//                   break;
+//                   case 9: case 10: case 11:
+//                   console.log("Kuz");
+//                   break;
+//                   default: console.log("Bunday raqamdagi oy mavjud emas ")
+//       }
+// }
+
+// M 4
+
+// let month = +prompt(
+//   "Yil oylari raqaimini kiriting, men esa uni qaysi oy va nechi kundan iborat ekanligini aytaman!"
+// );
+// {
+//   switch (month) {
+//     case 1:
+//       console.log("Yanvar oyi 31 kundan iborat");
+//       break;
+//     case 2:
+//       console.log("Fevral oyi 28 kun, 4 yilda bir marta 29 kundan iborat");
+//       break;
+//     case 3:
+//       console.log("Mart oyi 31 kundan iborat");
+//       break;
+//     case 4:
+//       console.log("Aprel oyi 30 kundan iborat");
+//       break;
+//     case 5:
+//       console.log("May oyi 31 kundan iborat");
+//       break;
+//     case 6:
+//       console.log("Iyun oyi 30 kundan iborat");
+//       break;
+//     case 7:
+//       console.log("Iyul oyi 31 kundan iborat");
+//       break;
+//     case 8:
+//       console.log("Avgust oyi 31 kundan iborat");
+//       break;
+//     case 9:
+//       console.log("Sentabr oyi 30 kundan iborat");
+//       break;
+//     case 10:
+//       console.log("Oktabr oyi 30 kundan iborat");
+//       break;
+//     case 11:
+//       console.log("Noyabr oyi 30 kundan iborat");
+//       break;
+//     case 12:
+//       console.log("Dekabr oyi 31 kundan iborat");
+//       break;
+//   }
+// }
+
+// M 5
+
+// let a = +prompt("Ixtiyoriy A son"),
+//   b = +prompt("Ixtiyoriy B son"),
+//   c = prompt("Istalgan matematik amal kiritng");
+//   switch (c) {
+//     case "+":
+//       console.log(a + b);
+//       break;
+//     case "*":
+//       console.log(a * b);
+//       break;
+//     case "/":
+//       console.log(a % b);
+//       break;
+//     case "-":
+//       console.log(a - b);
+//       break;
+//     case "**":
+//       console.log(a ** b);
+//       break;
+//     default:
+//       console.log("Bunday amal mavjud emas");
+//   }
+
+
+// M 6
+
+
+// let baxo=+prompt("Baxoni kiriting");
+// switch (baxo){
+//       case 1:
+//             console.log("Yomon baxo, stipendiya berilmaydi");
+//             break;
+//             case 2:
+//             console.log("Qoniqarsiz baxo, stipendiya 100 000 som");
+//             break;
+//             case 3:
+//             console.log("Qoniqarli baxo, stipendiya 250 000 som");
+//             break;
+//             case 4:
+//             console.log("Yaxshi baxo, stipendiya 450 000");
+//             break;
+//             case 5:
+//             console.log("A'lo baxo, stipendiya 600 000 som");
+//             break;
+//             default: console.log("Bunday baxo uchun ajratiladigan stipendiya ma'lum emas, bu darajaga qanday erishdingiz");
+
+
+
+
+
+
+
